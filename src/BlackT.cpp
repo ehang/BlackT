48fbf49a13bbc44be54470b2a4a31b9987c35408
ehang <uehang@gmail.com>
2019-12-21 01:01:12 +0800

    修改想法

---
+++ src/BlackT.cpp
@@ -0,0 +1,17 @@
+/*****************************************************************
+    > File Name: BlackT.cpp
+    > Author: uehang
+    > Mail: uehang@gmail.com 
+    > Created Time: Sun Aug 12 03:35:38 2018
+ ****************************************************************/
+#include<stdio>
+
+use namespace std;
+
+int main(int argc, char **agrv){
+	
+	//主线程接收棋局信息
+	//进行日志打印
+	//子线程获取棋局信息后，启动相应的进程
+	return 0;
+}
